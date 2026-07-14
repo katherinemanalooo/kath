8df049cbfa33e411bed41315687a99e21d9bab25
Katherine Manalo <manalotaji@gmail.com>
2026-07-14 13:04:46 +0800

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,70 @@
 import 'package:flutter/cupertino.dart';
+import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await LiquidGlassWidgets.initialize();
+  runApp(LiquidGlassWidgets.wrap(child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   bool value = false;
-
-  Future<void> ledLight(bool state) async {
+  Future<void> led_light(bool desmer) async{
+    String i = "";
+    if (desmer) {
+      i = "on";
+    }
+    else{
+      i = "off";
+    }
+    final link = "http://192.168.4.1/api/status/";
     final response = await http.post(
-      Uri.parse("http://192.168.4.1/api/status/"),
-      body: {
-        "state": state ? "on" : "off",
-      },
+        Uri.parse(link),
+        body:
+        {
+          "state" : i,
+        }
     );
-
-    debugPrint(response.body);
+    print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      theme: const CupertinoThemeData(
-        brightness: Brightness.dark,
-      ),
-      home: CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(
-          middle: Text("ESP32 LED Control"),
-        ),
-        child: SafeArea(
-          child: ListView(
-            children: [
-              CupertinoListSection.insetGrouped(
-                children: [
-                  CupertinoListTile(
-                    title: const Text("LED NI SIR RONNIE"),
-                    trailing: CupertinoSwitch(
-                      value: value,
-                      onChanged: (led) {
-                        setState(() {
-                          value = led;
-                        });
+      home: GlassScaffold(body: SafeArea(child: ListView(children: [
+        CupertinoListSection.insetGrouped(
+          backgroundColor: CupertinoColors.transparent,
+          children: [
+            GlassContainer(child: GlassListTile(title: Text('LED NI SIR RONNIE'),
+                trailing: GlassSwitch(value: value, onChanged: (led){setState(() {
+                  value = led;
+                  led_light(value);
+                  CupertinoListSection.insetGrouped(
+                    backgroundColor: CupertinoColors.transparent,
+                    children: [
+                      GlassContainer(child: GlassListTile(title: Text('LED NI SIR RONNIE'),
+                          trailing: GlassSwitch(value: value, onChanged: (led){setState(() {
+                            value = led;
+                            led_light(value);
+                          });
 
-                        ledLight(led);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+                          })),
+                      )
+                    ],);
+                });
+                })),
+            )
+          ],)
+      ],))),
     );
   }
 }
